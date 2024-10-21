69fb3771abaf7894ed4eafa88f63f7d12d33ede3
Gustavo Solano <e_geperez1@stefanini.com>
2024-10-21 16:09:23 -0600

    feat: se agrega index de sub divisores

--- src/app/home/constants/home-subdivisor-data.contants.ts
+++ src/app/home/constants/home-subdivisor-data.contants.ts
@@ -2,5 +2,10 @@ import { subDivisorData } from "../../shared/interface";
 
 export const HOME_SUBDIVISOR_DATA_WEB_APPS:subDivisorData = {
     title:'Aplicaciones WEB',
-    sections:[]
+    sections:[
+        {
+            title:'Velas y Veladoras de Oaxaca',
+            routerLink:''
+        }
+    ]
 }
--- src/app/shared/components/home-sub-divisor/components/home-sub-divisor-description/home-sub-divisor-description.component.ts
+++ src/app/shared/components/home-sub-divisor/components/home-sub-divisor-description/home-sub-divisor-description.component.ts
@@ -1,10 +1,13 @@
 import { Component, input } from '@angular/core';
 import { subDivisorData } from '../../../../interface';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home-sub-divisor-description',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './home-sub-divisor-description.component.html',
   styleUrl: './home-sub-divisor-description.component.scss'
 })
