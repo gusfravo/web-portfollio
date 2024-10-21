60ac5da8ab877c8693b361287831e0e1eb9739cd
Gustavo Solano <e_geperez1@stefanini.com>
2024-10-21 13:39:47 -0600

    feat se agrega icongrafia de habilidades

--- src/app/home/home-about-me/enums/home-about-me-icons.enum.ts
+++ src/app/home/home-about-me/enums/home-about-me-icons.enum.ts
@@ -2,5 +2,13 @@ export enum HomeAboutMeIcons {
     html = 'assets/icons/html.svg',
     javascript = 'assets/icons/javascript-js.svg',
     typescript = 'assets/icons/typescript.svg',
-    
+    android = 'assets/icons/android.svg',
+    ios = 'assets/icons/ios.svg',
+    angular = 'assets/icons/angular.svg',
+    capacitor = 'assets/icons/capacitor.svg',
+    cordova = 'assets/icons/cordova.svg',
+    laravel = 'assets/icons/laravel.svg',
+    node = 'assets/icons/node-js.svg',
+    godot = 'assets/icons/godot.svg',
+    mysql = 'assets/icons/mysql.svg'
   }
